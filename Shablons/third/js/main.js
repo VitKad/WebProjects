3e75413d335845d6d6d92110cc51727fd341b280
Victor Kadyko <victor_-kadiko@mail.ru>
2021-05-11 19:17:02 +0300

    adaptive

--- Shablons/third/js/main.js
+++ Shablons/third/js/main.js
@@ -3,6 +3,23 @@ $(function () {
         slidesToShow: 3,
         slidesToScroll: 3,
         dots: true,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 864,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+
+        ]
     });
 });

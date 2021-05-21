9cf71cc25cb26d83e144c0842f1a57e7f439db79
Victor Kadyko <victor_-kadiko@mail.ru>
2021-05-21 19:57:52 +0300

    rebuild gulp-start project

---
+++ Shablons/fourth/app/js/slick.js
@@ -0,0 +1,29 @@
+$('.header__carousel').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+
+});
+
+$('.resources__carousel').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+
+});
+
+$('.reviews__carousel').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+
+});
+
+
+
+ 

1ab7510ed01491d2fce49659f1569d280061abe0
Victor Kadyko <victor_-kadiko@mail.ru>
2021-09-06 22:09:19 +0300

    edit slider on photo

--- Shablons/photo/app/js/main.js
+++ Shablons/photo/app/js/main.js
@@ -69,7 +69,13 @@ $(function () {
     });
 
 
-
+    $('.reviews__slider').slick({
+        arrows:false,
+        dots:true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+ 
+    });
     
 
 

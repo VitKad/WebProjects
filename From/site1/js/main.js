15f5e25e7ce6c5c5b8d4c55543e71e29b1c0a79a
VitKad <74490554+VitKad@users.noreply.github.com>
2022-07-08 22:17:51 +0300

    full adaptive

--- From/site1/js/main.js
+++ From/site1/js/main.js
@@ -4,7 +4,15 @@ $(function(){
   $('.slider-blog__inner').slick({
    dots:true,
    prevArrow:'<button type="button" class="slick-prev"><img src="images/prev-arrow.svg" alt="prev"></button>',
-   nextArrow:'<button type="button" class="slick-next"><img src="images/next-arrow.svg" alt="next"></button>'
+   nextArrow:'<button type="button" class="slick-next"><img src="images/next-arrow.svg" alt="next"></button>',
+   responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        arrows: false
+      }
+    }
+  ]
   });
     
 

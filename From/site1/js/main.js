fef914f1430e6eed301b045499a2ed14c30a18bc
VitKad <74490554+VitKad@users.noreply.github.com>
2022-07-08 13:58:47 +0300

    add slider-blog

--- From/site1/js/main.js
+++ From/site1/js/main.js
@@ -1,5 +1,11 @@
 $(function(){
   
   var mixer = mixitup('.portfolio__content');
+
+  $('.slider-blog__inner').slick({
+   dots:true,
+   prevArrow:'<button type="button" class="slick-prev"><img src="images/prev-arrow.svg" alt="prev"></button>',
+   nextArrow:'<button type="button" class="slick-next"><img src="images/next-arrow.svg" alt="next"></button>'
+  });
     
 });

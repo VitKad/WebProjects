dc221d61c76a67c5b101158570cd36f9e5576650
VitKad <74490554+VitKad@users.noreply.github.com>
2022-07-12 23:30:47 +0300

    1/2 adaptive

--- Shablons/training/app/js/main.js
+++ Shablons/training/app/js/main.js
@@ -1,8 +1,23 @@
 $(function(){
 
+
+  $('.menu a, .logo').on('click', function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top}, 1500);
+  });
+
   $('.blog__slider').slick({
    arrows:false,
    dots: true
+  });
+
+
+
+  $(".menu__btn, .menu__link").on("click", function(){
+    $(".menu").toggleClass("menu--active");
+  
   });
 
 

d7ebccadd0fa540d5ad1ebe9e69b500517ac727a
VitKad <74490554+VitKad@users.noreply.github.com>
2022-07-25 12:27:51 +0300

    edit gulp-start

--- Gulp/Gulp-start/gulpfile.js
+++ Gulp/Gulp-start/gulpfile.js
@@ -68,8 +68,8 @@
             server: {
                 baseDir: "app/"
             },
-            notify: false,
-            host: devip() 
+             notify: false,
+             host: devip() 
         });
     }
 
@@ -77,6 +77,11 @@
     function scripts(){
         return src([
             'node_modules/jquery/dist/jquery.js',
+           /*  'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+            'node_modules/rateyo/src/jquery.rateyo.js',
+            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+            'node_modules/jquery-form-styler/dist/jquery.formstyler.js', */
             'app/js/main.js'
         ])
         .pipe(concat('main.min.js'))

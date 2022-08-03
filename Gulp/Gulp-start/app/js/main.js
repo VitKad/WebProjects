db4530de3a40515bdacb01fbfaf54531b01ec47b
VitKad <74490554+VitKad@users.noreply.github.com>
2022-08-03 16:57:47 +0300

    edit gulp start file

--- Gulp/Gulp-start/app/js/main.js
+++ Gulp/Gulp-start/app/js/main.js
@@ -1,5 +1,12 @@
 $(function(){
 
+  /* $('.article__navigation-link').on('click', function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top}, 1000);
+  }); */
+
 
 
 });
--- Gulp/Gulp-start/gulpfile.js
+++ Gulp/Gulp-start/gulpfile.js
@@ -77,11 +77,14 @@
     function scripts(){
         return src([
             'node_modules/jquery/dist/jquery.js',
-           /*  'node_modules/slick-carousel/slick/slick.js',
-            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-            'node_modules/rateyo/src/jquery.rateyo.js',
+            /* 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+            'node_modules/slick-carousel/slick/slick.js',        
             'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-            'node_modules/jquery-form-styler/dist/jquery.formstyler.js', */
+            'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+            'node_modules/rateyo/src/jquery.rateyo.js',
+            'node_modules/mixitup/dist/mixitup.js',
+            'node_modules/jquery-parallax.js/parallax.js', 
+            'node_modules/accordionjs/accordion.js',  */
             'app/js/main.js'
         ])
         .pipe(concat('main.min.js'))

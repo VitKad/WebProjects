ae8e7f1e22f8c2d5e1102fe7e1a1cb478f8022d9
VitKad <74490554+VitKad@users.noreply.github.com>
2022-08-06 23:03:50 +0300

    added ufo site

---
+++ Shablons/ufo/app/js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+    $('.carousel__inner-block').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 864,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+
+        ]
+    });
+});

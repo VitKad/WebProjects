4a1e229f374d57c1b248afb341095b5c204bc3b5
VitKad <74490554+VitKad@users.noreply.github.com>
2022-08-18 17:23:45 +0300

    create portfolio site project

---
+++ Shablons/Portfolio/app/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  /* $('.article__navigation-link').on('click', function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top}, 1000);
+  }); */
+
+  /* new WOW().init(); */
+
+});
+  
+
